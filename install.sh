a6cbba41195de2c5ee1edfd738f753ab8b058cce
Julian Reder <jjreder@gmail.com>
2023-06-11 11:50:29 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,9 @@ curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sudo apt-get install -f
 sudo cp airstrikd.service /etc/systemd/system/airstrikd.service
 sudo chmod 644 /etc/systemd/system/airstrikd.service
 systemctl daemon-reload
 python3 -m pip install -r requirements.txt
-echo "Run system
-
-ctl start airstrikd to begin"
+echo "Run systemctl start airstrikd to begin"
