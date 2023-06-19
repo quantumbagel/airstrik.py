34c2a6f313fde7a1286bc04e1785b45115d22d78
Julian Reder <jjreder@gmail.com>
2023-06-19 12:20:31 -0400

    ITS GOOD

--- airstrik_mongo.py
+++ airstrik_mongo.py
@@ -415,7 +415,6 @@ def collect_data(aircraft_json, plane_history):
                         continue
                     dw = False
                     for kval in plane_history[aircraft['hex']][item][::-1]:
-                        print(item, kval)
                         if kval[1] <= closest_time:
                             write.update({item.replace('_history', ''): kval})
                             dw = True
