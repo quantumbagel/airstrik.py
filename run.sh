818209ac4a62de947f9d659204e62e81ad8e5114
Julian Reder <jjreder@gmail.com>
2023-06-19 13:28:58 -0400

    Added run.sh beta

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting dump1090..."
+rm -rf dump1090/airstrik_data_runner_0
+mkdir dump1090/airstrik_data_runner_0
+sudo ./dump1090/dump1090 --write-json airstrik_data_runner_0 --write-json-every 0 &
+python3 airstrik_mongo.py --no-start-dump1090 airstrik_data_runner_0 --no-purge --database-out airstrikd0_out --log-mode
