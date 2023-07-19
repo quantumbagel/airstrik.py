526e53a0872f8a35756cf769d676cbdee7cf5755
quantumbagel <jjreder@gmail.com>
2023-07-19 10:21:44 -0400

    Fixed index bug i think

--- airstrik_mongo.py
+++ airstrik_mongo.py
@@ -455,7 +455,7 @@ def collect_data(aircraft_json, plane_history):
                             write.update({item.replace('_history', ''): None})
                 write['extras'] = {'start_time': ac_dt['extras']['start_time']}
                 write['extras'].update({"end_time": aircraft_json['now']})
-                matched_filters = match_filters(write['distance'], write['alt_geom'])
+                matched_filters = match_filters(write['distance'][0], write['alt_geom'][0])
                 write['filters'] = matched_filters
                 database.database[aircraft['hex']].insert_one(write)
             else:
